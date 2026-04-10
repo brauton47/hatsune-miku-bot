f7e4dd8059a1f21f5fce275edb2d8d6240b46c9a
brauton47 <cristianbrauton@gmail.com>
2026-04-10 10:58:10 -0300

    config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+global.owner = ['5492281367797'] // Pon tu número aquí mi amor ❤️
+global.mods = [] 
+global.prems = [] 
+
+global.APIs = {
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+
+global.APIKeys = {
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+}
+
+global.packname = 'Miku-MD'
+global.author = 'Yoel' // Yoel 😘
+global.wm = '© LoliBot - MD'
+
+global.multiplier = 69
